a69a239efa025a38c1ce98047453e064f6fe45f9
İsa <ulutepe42@gmail.com>
2023-12-18 13:13:37 +0300

    First Commit

---
+++ app_api/models/db.js
@@ -0,0 +1,20 @@
+var mongoose  =require("mongoose");
+var dbURI= "mongodb://127.0.0.1/mekanbul";
+mongoose.connect(dbURI);
+mongoose.connection.on("connected", function(){
+    console.log("Bağlandı");
+});
+mongoose.connection.on("error", function(){
+    console.log("Bağlantıda hata var");
+});
+mongoose.connection.on("disconnected", function(){
+    console.log("Her eyes left school my mind is bad");
+});
+process.on("SIGINT" ,function(){
+    mongoose.connection.close();
+    console.log("Uygulama kapatıldı");
+    process.exit(0);
+});
+require("./venue"); 
+
+//Şifre:byqvEpmckUz14vQR
